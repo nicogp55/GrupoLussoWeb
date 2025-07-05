aa1bda8101748b36a9a9c64839a24bd9ebbee6a0
Nicolás Grosso <personal@MacBook-Pro-de-Nicolas-2.local>
2025-07-05 18:33:25 -0300

    V1.001

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+
+    // 👇 Añade esta línea
+    output: 'export',
 };
 
 export default nextConfig;
