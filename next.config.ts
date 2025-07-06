49a13a883f1ded19aa251f7aa7a79e49338cfe16
Nicolás Grosso <personal@MacBook-Pro-de-Nicolas-2.local>
2025-07-06 08:11:03 -0300

    Limpieza final

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-
-    // 👇 Añade esta línea
-    output: 'export',
 };
 
 export default nextConfig;
